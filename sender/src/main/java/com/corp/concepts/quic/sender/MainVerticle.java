2942f3c06caa07636629746868990cfa24b20e4c
selcuksert <selcuk.sert@gmail.com>
2021-11-20 21:51:22 +0300

    Removed receiver

--- sender/src/main/java/com/corp/concepts/quic/sender/MainVerticle.java
+++ sender/src/main/java/com/corp/concepts/quic/sender/MainVerticle.java
@@ -3,8 +3,10 @@ package com.corp.concepts.quic.sender;
 import com.corp.concepts.quic.sender.handlers.HttpQuic;
 import com.corp.concepts.quic.sender.handlers.Stream;
 import com.corp.concepts.quic.sender.handlers.Welcome;
+import io.vertx.config.ConfigRetriever;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Promise;
+import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.Router;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -23,15 +25,23 @@ public class MainVerticle extends AbstractVerticle {
 
         router.route("/http").handler(new HttpQuic());
 
-        vertx.createHttpServer().requestHandler(router)
-                .listen(8888, http -> {
-                    if (http.succeeded()) {
-                        startPromise.complete();
-                        logger.info("HTTP server started on port 8888");
-                    } else {
-                        startPromise.fail(http.cause());
-                    }
-                });
+        ConfigRetriever retriever = ConfigRetriever.create(vertx);
+
+        retriever.getConfig(json -> {
+            JsonObject result = json.result();
+            JsonObject server = result.getJsonObject("server");
+            int port = server.getInteger("port");
+
+            vertx.createHttpServer().requestHandler(router)
+                    .listen(port, http -> {
+                        if (http.succeeded()) {
+                            startPromise.complete();
+                            logger.info("HTTP server started on port: {}", http.result().actualPort());
+                        } else {
+                            startPromise.fail(http.cause());
+                        }
+                    });
+        });
     }
 
 }
